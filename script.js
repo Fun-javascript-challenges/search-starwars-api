31a9f1611ce426de760253b4899006305a2afd90
timfel20 <timmyfelicia@gmail.com>
2023-04-30 20:43:39 +0200

    planet category and search functionality

--- script.js
+++ script.js
@@ -1,67 +1,167 @@
 let apiCardsFilm = document.getElementById('api-cards-film')
 let filmsURL = 'https://swapi.dev/api/films/';
 
+
+
+
 fetch(filmsURL)
 .then(Response => Response.json())
 .then(data => {
     let films = data.results
-   /*  console.log(films) */
-    for (let i = 0; i < films.length; i++) {
-       /*  console.log(films[i]);  */
+    for (let i = 0; i <= 4; i++) {
          
-    const imageUrl = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`;
-    /* console.log(imageUrl) */
+        const imageUrl = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`;
+        /* console.log(imageUrl) */
 
-    apiCardsFilm.innerHTML += `
-    <div class="card">
-        <div class="card-front">
-            <p class="title">${films[i].title}</p>
-            <div class="api-card-img-container"><img src = ${imageUrl}></div>
-        </div>
-        <div class="card-back">
-            <p>
-                ${films[i].title} is a movie in the Star Wars universe. Created at ${films[i].created}
-                and directed by ${films[i].director}, a well known director. The film was finally released
-                on ${films[i].release_date}. It has been one of a kind ever since 
-            </p>
-        </div>
-    </div>
-    ` 
-    }
-   
+        let film = films[i];
 
-   
+        apiCardsFilm.innerHTML += `
+        <div class="card" id="card-flm">
+            <div class="card-front">
+                <p class="title">${film.title}</p>
+                <div class="api-card-img-container"><img src = ${imageUrl}></div>
+            </div>
+            <div class="card-back">
+                <p>
+                    ${film.title} is a movie in the Star Wars universe. Created at ${film.created}
+                    and directed by ${film.director}, a well known director. The film was finally released
+                    on ${film.release_date}. It has been one of a kind ever since 
+                </p>
+            </div>
+        </div>
+        ` 
+
+       
+        let cards = document.querySelectorAll('.card')
+        
+
+        searchItems(cards)  
+    }   
+ 
+    
 });
 
 
 let apiCardsCharacters = document.getElementById('api-cards-people');
 let charactersURL = 'https://swapi.dev/api/people/';
+let character = ""
 
 fetch(charactersURL)
 .then(Response => Response.json())
 .then(data => {
     let characters = data.results
-    console.log(characters)
     for (let i = 0; i < characters.length; i++) {
-        console.log(characters[i]); 
-         
-    const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${i + 1}.jpg`;
-    console.log(imageUrl)
+       /*  console.log(characters[i]);  */
 
-    apiCardsCharacters.innerHTML += `
-    <div class="card">
-        <div class="card-front">
-            <p class="title">${characters[i].name}</p>
-            <div class="api-card-img-container"><img src = ${imageUrl}></div>
+        let character = characters[i];
+            
+        const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${i + 1}.jpg`;
+        /* console.log(imageUrl) */
+
+        apiCardsCharacters.innerHTML += `
+        <div class="card" id="card">
+            <div class="card-front">
+                <p class="title">${character.name}</p>
+                <div class="api-card-img-container"><img src = ${imageUrl}></div>
+            </div>
+            <div class="card-back">
+                <p>
+                ${character.name} is ${character.gender === 'n/a' ? 'of unknown gender ' : `a ${character.gender}`} born in 
+                ${character.birth_year}. Known to have ${character.skin_color} skin color, 
+                ${character.eye_color} eyes, and stand at a height of ${character.height}cm.
+                </p>
+            </div>
         </div>
-        <div class="card-back">
-            <p>
-            ${characters[i].name} is ${characters[i].gender === 'n/a' ? 'of unknown gender ' : `a ${characters[i].gender}`} born in 
-            ${characters[i].birth_year}. Known to have ${characters[i].skin_color} skin color, 
-            ${characters[i].eye_color} eyes, and stand at a height of ${characters[i].height}cm.
-            </p>
-        </div>
-    </div>
-    ` 
-    }   
+        ` 
+        let cards = document.querySelectorAll('.card')
+
+        searchItems(cards)
+       
+    } 
+   
+     
+        /* console.log(characters[i]) */
+    
+    
 });
+
+let apiCardsPlanets = document.getElementById('api-cards-planet');
+let planetsURL = 'https://swapi.dev/api/planets/';
+let planet = ""
+
+fetch(planetsURL)
+.then(Response => Response.json())
+.then(data => {
+    let planets = data.results
+    for (let i = 0; i < planets.length; i++) {
+       /*  console.log(planets[i]);  */
+
+        let planet = planets[i];
+            
+        const imageUrl = `https://starwars-visualguide.com/assets/img/planets/${i + 1}.jpg`;
+        /* console.log(imageUrl) */
+
+        apiCardsPlanets.innerHTML += `
+        <div class="card" id="card">
+            <div class="card-front">
+                <p class="title">${planet.name}</p>
+                <div class="api-card-img-container"><img src = ${imageUrl}></div>
+            </div>
+            <div class="card-back">
+                <p>
+                planet ${planet.name}, with a diameter of ${planet.diameter} and a terrain type ${planet.terrain}. 
+                The climate of the planet is ${planet.climate}, with a population of ${planet.population}
+                </p>
+            </div>
+        </div>
+        ` 
+        let cards = document.querySelectorAll('.card')
+
+        searchItems(cards)
+       
+    } 
+   
+     
+        /* console.log(characters[i]) */
+    
+    
+});
+
+/* functionality for search */
+
+    function searchItems(cards){
+        let searcher = document.getElementById("search-input");
+        
+        searcher.addEventListener("input", function(e){
+            e.preventDefault()
+            
+            let searchWord = searcher.value.toLowerCase();
+            
+            cards.forEach(card => {
+                let title = "";
+                let name = "";
+                let planet = "";
+    
+                if (card.querySelector(".title")) {
+                    title = card.querySelector(".title").textContent.toLowerCase();
+                } else if (card.querySelector(".name")) {
+                    name = card.querySelector(".name").textContent.toLowerCase();
+                }
+                else if (card.querySelector(".planet")) {
+                    planet = card.querySelector(".name").textContent.toLowerCase();
+                
+                }
+
+                if (title.includes(searchWord) || name.includes(searchWord)) {
+                    card.style.display = "block"; 
+                } else {
+                    card.style.display = "none"; 
+                }
+            });
+        });
+    }
+ 
+
+       
+   
+
